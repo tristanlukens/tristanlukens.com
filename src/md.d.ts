0514caba48d1d51a495a5e129f0ad2f56fe6d8d4
Tristan Lukens <71601628+tristanlukens@users.noreply.github.com>
2025-07-31 18:49:48 +0200

    Added markdown inclusion for vite

---
+++ src/md.d.ts
@@ -0,0 +1,1 @@
+declare module '$lib/data/home.md';
--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,9 +1,6 @@
 import { read } from '$app/server';
 
-// I'm importing a txt file with markdown syntax, because apparently Svelte doesn't
-// support importing a .md file. Wish I could've found that out sooner, because it
-// took a lot of trial and error.
-import md from './home.txt';
+import md from '$lib/data/home.md';
 
 import type { PageServerLoad } from './$types';
 
