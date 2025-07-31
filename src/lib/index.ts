59e7ed07bcf3a8839c9f1bded45b02eabfcce2b2
Tristan Lukens <71601628+tristanlukens@users.noreply.github.com>
2025-07-31 18:50:38 +0200

    Deleted unneeded index.ts

--- src/lib/index.ts
+++
@@ -1,7 +0,0 @@
-// place files you want to import through the `$lib` alias in this folder.
-import { read } from '$app/server';
-
-export const readMarkdown = async (path: string) => {
-	const asset = read(path);
-	return await asset.text();
-};
